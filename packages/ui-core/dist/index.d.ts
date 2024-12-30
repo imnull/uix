99eb3334ef4cd0e1a2c8e7fb4166d8c666c1248b
marvin <imnull@outlook.com>
2024-12-30 08:18:56 +0800

    feat: git ignore dist

--- packages/ui-core/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { TPoint } from './utils';
-export declare const initGestureEvents: (element: HTMLElement | string, options: {
-    onStart: () => void;
-    onMove: (p: TPoint) => void;
-    onEnd: () => void;
-}) => {
-    init: () => void;
-    dispose: () => void;
-};
