8271a923e396dccbf08e258de4abad20d40f5941
marvin <imnull@outlook.com>
2025-01-18 15:03:26 +0800

    feat: build for npm

--- packages/swiper-vue3/vite.config.ts
+++ packages/swiper-vue3/vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
+import path from 'path'
+import { defineConfig, LibraryOptions } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [vue(), vueJsx()],
-  build: {
-    outDir: path.resolve(__dirname, '../../docs/swiper-vue3'),
+export default defineConfig(options => {
+  const { mode } = options
+  const outDir = mode === 'DOC' ? path.resolve(__dirname, '../../docs/swiper-vue3') : 'dist'
+  const lib: LibraryOptions | false = mode === 'DOC' ? false : {
+    entry: './src/components/index.ts',  // 入口文件，通常是组件库的主入口
+    name: '@imnull/swiper-vue3', // 组件库名称
+    fileName: 'swiper',
+    // fileName: (format) => `my-vue-component-library.${format}.js`, // 输出文件名称
+    formats: ['es', 'cjs'], // 打包格式
+  }
+  return {
+    base: './',
+    plugins: [vue(), vueJsx()],
+    build: {
+      emptyOutDir: true,
+      lib,
+      outDir,
+    }
   }
 })
